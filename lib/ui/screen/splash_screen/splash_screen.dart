f12be34ad513c53b9e668935cef0f972a2be418c
Mario Monzón <mario.monzon.bitbox@gmail.com>
2022-01-13 14:34:46 +0100

    create home screen

---
+++ lib/ui/screen/splash_screen/splash_screen.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:lottie/lottie.dart';
+import 'package:sport_team_manager/generated/assets.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({Key? key}) : super(key: key);
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  @override
+  void initState() {
+    Future.delayed(const Duration(seconds: 4),
+        () => Navigator.of(context).pushReplacementNamed('/homescreen'));
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Lottie.asset(Assets.lottieKickOff),
+      ),
+    );
+  }
+}
