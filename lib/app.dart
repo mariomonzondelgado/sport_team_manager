1559b3a11e42fefc41647c81c0a11b3ca49ae397
Mario Monzón <mario.monzon.bitbox@gmail.com>
2022-01-13 16:57:14 +0100

    Create tabs screen

--- lib/app.dart
+++ lib/app.dart
@@ -1,20 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:sport_team_manager/ui/screen/tabs_screen/tabs_screen.dart';
 
 class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Material App',
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('Material App Bar'),
-        ),
-        body: Center(
-          child: Container(
-            child: Text('Hello World'),
-          ),
-        ),
-      ),
+      home: TabsScreen(),
     );
   }
 }
