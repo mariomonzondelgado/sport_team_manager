33dfbaa333376ad9bc176ce1adec530d89cbdc38
Mario Monzón <mario.monzon.bitbox@gmail.com>
2022-01-14 15:49:05 +0100

    Add fake data

--- lib/util/fake_data.dart
+++ lib/util/fake_data.dart
@@ -10,3 +10,8 @@ const String fakeCardBody =
     'efficitur. Aliquam at accumsan enim, luctus pulvinar justo.';
 const String fakeEventCardBody = 'Partido de liga que enfrentara a Telde Canes '
     'contra el equipo de la isla vecina Tenerife Helldogs';
+const String fakeSponsorName = 'BitBox SL';
+const String fakeSponsorDescription =
+    'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed commodo '
+    'vestibulum est vel accumsan. Vestibulum nec lectus ligula. Etiam pretium '
+    'tempus leo a ultrices. Aliquam interdum accumsan erat. Duis vel neque ';
