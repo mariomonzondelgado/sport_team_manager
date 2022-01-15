e0db5f00338fe06b6d91bc2237f0883375c8eb29
Mario Monzón <hola@mariomonzon.dev>
2022-01-15 13:01:56 +0000

    Create export screens files

---
+++ lib/ui/screens.dart
@@ -0,0 +1,4 @@
+export 'package:sport_team_manager/ui/screen/home_screen/home_screen.dart';
+export 'package:sport_team_manager/ui/screen/login_screen/login_screen.dart';
+export 'package:sport_team_manager/ui/screen/register_screen/register_screen.dart';
+export 'package:sport_team_manager/ui/screen/splash_screen/splash_screen.dart';
