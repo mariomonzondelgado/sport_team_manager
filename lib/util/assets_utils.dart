900dda01dabae9fa5d005d2d5aa719ad9636da75
Mario Monzón <hola@mariomonzon.dev>
2022-01-16 21:51:28 +0000

    Create assets util

---
+++ lib/util/assets_utils.dart
@@ -0,0 +1,8 @@
+class Res {
+  Res._();
+
+  static const String imagesFootball = 'assets/images/football.jpg';
+  static const String imagesPlaceholder = 'assets/images/placeholder.jpg';
+  static const String imagesLogoCanes = 'assets/images/logocanes2.png';
+  static const String lottieKickOff = 'assets/lottie/kick_off.json';
+}
