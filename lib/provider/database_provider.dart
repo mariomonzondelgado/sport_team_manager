7a9c58184a0e84d496f1d69b05ddadd0d18a1876
Mario Monzón <mario.monzon.bitbox@gmail.com>
2022-01-19 13:18:51 +0100

    Change riverpod import for flutter_riverpod import

--- lib/provider/database_provider.dart
+++ lib/provider/database_provider.dart
@@ -1,4 +1,4 @@
-import 'package:riverpod/riverpod.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sport_team_manager/service/database.dart';
 
 final databaseProvider = Provider((ref) => Database());
