2dea981bd048ca79e8f6e99e95de4fdb1023b783
Mario Monzón <mario.monzon.bitbox@gmail.com>
2022-01-19 13:19:08 +0100

    Implement StreamBuilder

--- lib/ui/screen/news_tab_screen/news_tab_screen.dart
+++ lib/ui/screen/news_tab_screen/news_tab_screen.dart
@@ -1,14 +1,37 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:sport_team_manager/ui/screen/news_tab_screen/widget/post_card_widget.dart';
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:sport_team_manager/provider/database_provider.dart';
 
-class NewsTabScreen extends StatelessWidget {
+class NewsTabScreen extends ConsumerWidget {
   const NewsTabScreen({Key? key}) : super(key: key);
 
   @override
-  Widget build(BuildContext context) {
-    return ListView.builder(
-      itemCount: 20,
-      itemBuilder: (context, int) => PostCardWidget(),
+  Widget build(BuildContext context, WidgetRef ref) {
+    final database = ref.watch(databaseProvider);
+
+    return StreamBuilder<QuerySnapshot>(
+      stream: database.allNews,
+      builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+        if (snapshot.hasError) {
+          return Text('Something went wrong');
+        }
+
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Text("Loading");
+        }
+
+        return ListView(
+          children: snapshot.data!.docs.map((DocumentSnapshot document) {
+            Map<String, dynamic> data =
+                document.data()! as Map<String, dynamic>;
+            return ListTile(
+              title: Text(data['title']),
+              subtitle: Text(data['body']),
+            );
+          }).toList(),
+        );
+      },
     );
   }
 }
