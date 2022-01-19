717b03aec9a232dd6979e2d02d5c95d4f5fa46a3
Mario Monzón <mario.monzon.bitbox@gmail.com>
2022-01-19 15:51:37 +0100

    Create url launcher util

---
+++ lib/util/url_launcher_util.dart
@@ -0,0 +1,36 @@
+import 'package:url_launcher/url_launcher.dart';
+
+import 'text_utils.dart';
+
+class UrlLauncherUtils {
+  static openMap(double lat, double lon) async {
+    final url = 'https://www.google.com/maps/search/?api=1&query=$lat,$lon';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not launch $url';
+    }
+  }
+
+  static call(String phoneNumber) async {
+    if (phoneNumber.isNotEmpty) {
+      final url = 'tel:+$phoneNumber'..replaceAll('++', '+');
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        throw 'Could not launch $url';
+      }
+    }
+  }
+
+  static mailTo(String email) async {
+    if (email.isNotEmpty && email.isEmail()) {
+      final url = 'mailto:$email';
+      if (await canLaunch(url)) {
+        await launch(url);
+      } else {
+        throw 'Could not launch $url';
+      }
+    }
+  }
+}
