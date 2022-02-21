df8163d6579a45dcc4ee57822a745a00f662a1db
Mario Monzón <hola@mariomonzon.dev>
2022-02-21 23:20:12 +0000

    Modify error handle

--- lib/service/auth_service.dart
+++ lib/service/auth_service.dart
@@ -1,15 +1,13 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:logger/logger.dart';
 
-
 class AuthService {
   final _auth = FirebaseAuth.instance;
-  Stream<User?> get authStatus=>_auth.authStateChanges();
-
+  Stream<User?> get authStatus => _auth.authStateChanges();
 
   Future<void> signup(String email, String password) async {
     try {
-       await _auth.createUserWithEmailAndPassword(
+      await _auth.createUserWithEmailAndPassword(
         email: email,
         password: password,
       );
@@ -25,21 +23,11 @@ class AuthService {
   }
 
   Future<void> signin(String email, String password) async {
-    try {
-        await FirebaseAuth.instance
-          .signInWithEmailAndPassword(email: email, password: password);
-    } on FirebaseAuthException catch (e) {
-      if (e.code == 'user-not-found') {
-        Logger().e('No user found for that email.');
-      } else if (e.code == 'wrong-password') {
-        Logger().e('Wrong password provided for that user.');
-      }
-    } catch (e) {
-      Logger().e(e);
-    }
+    await FirebaseAuth.instance
+        .signInWithEmailAndPassword(email: email, password: password);
   }
 
-  User? currentUser(){
+  User? currentUser() {
     final user = _auth.currentUser;
     if (user != null) {
       return user;
