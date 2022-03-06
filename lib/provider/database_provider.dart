d17a739877e6fc5f64f4c8f75f4d353468937e7f
Mario Monzón <hola@mariomonzon.dev>
2022-03-06 17:14:16 +0000

    Add storage provider

--- lib/provider/database_provider.dart
+++ lib/provider/database_provider.dart
@@ -1,4 +1,6 @@
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:sport_team_manager/service/database.dart';
+import 'package:sport_team_manager/service/storage_service.dart';
 
 final databaseProvider = Provider((ref) => Database());
+final storageServiceProvider = Provider((ref) => StorageService());
