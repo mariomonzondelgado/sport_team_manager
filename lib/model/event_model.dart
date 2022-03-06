5e5c5bb171fa603df1f2c9697301fdde856e7a73
Mario Monzón <hola@mariomonzon.dev>
2022-03-06 17:18:40 +0000

    Edit event model

--- lib/model/event_model.dart
+++ lib/model/event_model.dart
@@ -5,6 +5,7 @@ part 'event_model.g.dart';
 @JsonSerializable(explicitToJson: true)
 class Event {
   Event({
+    required this.eventId,
     required this.title,
     required this.body,
     required this.date,
@@ -13,6 +14,9 @@ class Event {
   factory Event.fromJson(Map<String, dynamic> json) => _$EventFromJson(json);
 
   Map<String, dynamic> toJson() => _$EventToJson(this);
+
+  @JsonKey(name: 'event_id')
+  String eventId;
 
   @JsonKey(name: 'title')
   String title;
--- lib/model/event_model.g.dart
+++ lib/model/event_model.g.dart
@@ -7,12 +7,14 @@ part of 'event_model.dart';
 // **************************************************************************
 
 Event _$EventFromJson(Map<String, dynamic> json) => Event(
+      eventId: json['event_id'] as String,
       title: json['title'] as String,
       body: json['body'] as String,
       date: DateTime.parse(json['date'] as String),
     );
 
 Map<String, dynamic> _$EventToJson(Event instance) => <String, dynamic>{
+      'event_id': instance.eventId,
       'title': instance.title,
       'body': instance.body,
       'date': instance.date.toIso8601String(),
